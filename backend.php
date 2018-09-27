b7b0252f66200d1b96be04a4bcb8bf954fb7993d
JuanPaulGim <31192470+JuanPaulGim@users.noreply.github.com>
2018-09-27 17:32:23 -0500

    Add files via upload

--- backend.php
+++ backend.php
@@ -1,6 +1,32 @@
 <?php
-	$mysqli = new mysqli('localhost','root','','mi_revista');
-	$mysqli -> set_charset("utf8");
-	$query = $mysqli -> query("SELECT * FROM ciudad");
-	$query2 = $mysqli -> query("SELECT * FROM revista");
+
+require_once './conexion_bd.php';
+	
+	// capturar información del formulario de busqueda
+	if(isset($_GET['ident'])){
+		$obj = $_GET['ident'];
+		$observerClass->getUser($mysqli,$obj);
+	}
+
+	class observerClass{
+	function getUser(){
+
+		$queryRevista = $mysqli -> query ("SELECT nombre, apellido FROM usuario WHERE id_usuario =".$obj."");
+
+		sql_fetch_array($queryRevista);
+		if(isset($_GET['ident'])){
+			if(count($valores) > 0){
+				echo "Hola <b>".$valores['nombre']."</b> ya te encuentras registrado";
+			}else{
+				echo "No estas registrado, por favor registrate antes de realizar una suscripción";
+			}
+		}
+
+		return fetch_array;
+	}
+
+	function insertUser($mysqli, $obj){}
+
+	function suscribeUser($mysqli, $obj){}
+}
 ?>
